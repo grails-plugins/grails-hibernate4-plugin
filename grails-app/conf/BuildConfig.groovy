e4bcc537d4133fad951fcb2bebdd94ec52771f3b
Lari Hotari <lari.hotari@sagire.fi>
2013-09-26 07:36:49 +0300

    update to snapshot version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,7 +22,7 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 
-		String datastoreVersion = '2.0.1.RELEASE'
+		String datastoreVersion = '2.0.2.BUILD-SNAPSHOT'
 
 		compile "org.grails:grails-datastore-gorm-hibernate4:$datastoreVersion"
 
