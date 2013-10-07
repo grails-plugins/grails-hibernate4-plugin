ca7e05b8282eaeb73c259d45110cb214fed4ca5a
Lari Hotari <lari.hotari@sagire.fi>
2013-10-07 09:18:38 +0300

    use release plugin 3.0.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,7 +39,7 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build ':release:3.0.0', ':rest-client-builder:1.0.3', {
+		build ':release:3.0.1', ':rest-client-builder:1.0.3', {
 			export = false
 		}
 
