81add84c6ded27cda3fea78d5bfbac516e1382b2
Burt Beckwith <burt@burtbeckwith.com>
2013-10-16 13:44:06 -0400

    test fix

--- test/unit/org/codehaus/groovy/grails/orm/hibernate/cfg/GrailsDomainBinderTests.groovy
+++ test/unit/org/codehaus/groovy/grails/orm/hibernate/cfg/GrailsDomainBinderTests.groovy
@@ -847,7 +847,7 @@ class AuthorClass {
 		publisherPluginMap.getName = { -> 'Publisher' }
 		def publisherPlugin = publisherPluginMap as org.codehaus.groovy.grails.plugins.GrailsPlugin
 
-		def pluginManagerMap = [:]
+		def pluginManagerMap = [setApplicationContext: { }]
 		def myPluginDomainClassNames = ['GadgetClass', 'MyPluginPersonClass']
 		def publisherPluginDomainClassNames = ['AuthorClass']
 		pluginManagerMap.getPluginForClass = { Class clz ->
