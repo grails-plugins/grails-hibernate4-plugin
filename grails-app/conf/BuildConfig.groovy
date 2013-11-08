d05b032de2a97cf33adc8a50ed485aa808abbf51
Lari Hotari <lari.hotari@sagire.fi>
2013-11-08 07:44:42 +0200

    update g-d-m dependency to 2.0.4.BUILD-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,7 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 
-		String datastoreVersion = '2.0.3.RELEASE'
+		String datastoreVersion = '2.0.4.BUILD-SNAPSHOT'
 
         compile "org.grails:grails-datastore-core:$datastoreVersion", 
                 "org.grails:grails-datastore-gorm:$datastoreVersion", 
