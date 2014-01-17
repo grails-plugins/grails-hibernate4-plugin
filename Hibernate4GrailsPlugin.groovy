74c2bad27254ea670fb824ec5ad4320f350790c7
Graeme Rocher <graeme.rocher@gmail.com>
2014-01-17 11:57:10 +0100

    update plugin version to 4.1.11.7

--- Hibernate4GrailsPlugin.groovy
+++ Hibernate4GrailsPlugin.groovy
@@ -22,7 +22,7 @@ import org.codehaus.groovy.grails.commons.AnnotationDomainClassArtefactHandler
  */
 class Hibernate4GrailsPlugin {
 
-	def version = '4.1.11.6'
+	def version = '4.1.11.7'
 	def grailsVersion = '2.3.2 > *'
 	def author = 'Burt Beckwith'
 	def title = 'Hibernate 4 for Grails'
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,7 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 
-		String datastoreVersion = '2.0.6.RELEASE'
+		String datastoreVersion = '2.0.7.RELEASE'
 
         compile "org.grails:grails-datastore-core:$datastoreVersion", 
                 "org.grails:grails-datastore-gorm:$datastoreVersion", 
