4b5d343e64ec6605500d4ade7cbcbf664b5dc7ca
Lari Hotari <lhotari@gopivotal.com>
2014-02-12 16:48:36 +0200

    Fix DataSourceTests

--- test/unit/org/codehaus/groovy/grails/orm/hibernate/DataSourceTests.groovy
+++ test/unit/org/codehaus/groovy/grails/orm/hibernate/DataSourceTests.groovy
@@ -1,5 +1,6 @@
 package org.codehaus.groovy.grails.orm.hibernate
 
+import javax.sql.PooledConnection
 import org.hibernate.engine.jdbc.spi.JdbcWrapper
 import org.springframework.jdbc.datasource.ConnectionProxy
 
@@ -16,34 +17,33 @@ class Flanglurb {}
 	void testConnectionTypes() {
 		def dc = ga.getDomainClass('Flanglurb').clazz
 		dc.withTransaction { s ->
-
 			def dataSource = appCtx.dataSource
 			def dataSourceUnproxied = appCtx.dataSourceUnproxied
 
-			 // the session's connection will be a Hibernate wrapper around the Spring wrapper
 			def sessionFactoryConnection = sessionFactory.currentSession.connection()
-
-			assertTrue sessionFactoryConnection instanceof JdbcWrapper
-			sessionFactoryConnection = sessionFactoryConnection.getWrappedObject()
 
 			assertTrue sessionFactoryConnection instanceof ConnectionProxy
 			sessionFactoryConnection = sessionFactoryConnection.getTargetConnection()
 
-			 // a connection from the datasource proxy will be a Spring wrapper around Hibernate's wrapper
+			assertTrue sessionFactoryConnection instanceof PooledConnection
+			sessionFactoryConnection = sessionFactoryConnection.connection
+
 			def dataSourceConnection = dataSource.connection
 
 			assertTrue dataSourceConnection instanceof ConnectionProxy
 			dataSourceConnection = dataSourceConnection.getTargetConnection()
 
-			assertTrue dataSourceConnection instanceof JdbcWrapper
-			dataSourceConnection = dataSourceConnection.getWrappedObject()
-
 			assertTrue dataSourceConnection instanceof ConnectionProxy
 			dataSourceConnection = dataSourceConnection.getTargetConnection()
+
+			assertTrue dataSourceConnection instanceof PooledConnection
+			dataSourceConnection = dataSourceConnection.connection
 
 			def unproxiedConnection = dataSourceUnproxied.connection
 			assertFalse unproxiedConnection instanceof ConnectionProxy
 			assertFalse unproxiedConnection instanceof JdbcWrapper
+			assertTrue unproxiedConnection instanceof PooledConnection
+			unproxiedConnection = dataSourceUnproxied.connection
 
 			assertTrue sessionFactoryConnection.is(dataSourceConnection)
 			assertFalse unproxiedConnection.is(dataSourceConnection)
