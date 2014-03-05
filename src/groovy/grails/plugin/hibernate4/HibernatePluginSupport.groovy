74cd100d31b7bcc0cd5ecc82544d610d2d7dd5f8
Lari Hotari <lhotari@gopivotal.com>
2014-03-05 09:07:46 +0200

    GRAILS-11177 add feature for configuring OSIV singleSession = false

--- src/groovy/grails/plugin/hibernate4/HibernatePluginSupport.groovy
+++ src/groovy/grails/plugin/hibernate4/HibernatePluginSupport.groovy
@@ -218,6 +218,7 @@ Using Grails' default naming strategy: '${ImprovedNamingStrategy.name}'"""
 
 				hibProps.putAll(hibConfig.flatten().toProperties('hibernate'))
 				hibProps.remove('hibernate.reload')
+				hibProps.remove('hibernate.singleSession')
 
 				// move net.sf.ehcache.configurationResourceName to "top level"	if it exists
 				if (hibProps.'hibernate.net.sf.ehcache.configurationResourceName') {
@@ -312,6 +313,10 @@ Using Grails' default naming strategy: '${ImprovedNamingStrategy.name}'"""
 					}
 
 					sessionFactory = ref("sessionFactory$suffix")
+
+					if(hibConfig?.containsKey('singleSession')) {
+						singleSession = hibConfig.singleSession as Boolean
+					}
 				}
 
 				if (getSpringConfig().containsBean("controllerHandlerMappings")) {
