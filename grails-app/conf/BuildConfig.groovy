1e3a3c57418b80188accbba54d6bc64dc995e010
Graeme Rocher <graeme.rocher@gmail.com>
2014-04-23 12:23:48 +0200

    use 3.0.6 release of datastore jars

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,7 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 
-        String datastoreVersion = '3.0.6.BUILD-SNAPSHOT'
+        String datastoreVersion = '3.0.6.RELEASE'
         String hibernateVersion = '4.3.5.Final'
 
         compile "org.grails:grails-datastore-core:$datastoreVersion",
